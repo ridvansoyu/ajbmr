5bc7b5f0217a4dc5f6091a165fb18295009b74cd
ridvansoyu <ridvansoyu@gmail.com>
2025-08-18 18:04:32 +0300

    add urls to settings

--- backend/core/settings/urls.py
+++ backend/core/settings/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def root_view(request):
+    return JsonResponse({"status": "backend running"})
 
 urlpatterns = [
+    path('', root_view),
     path('admin/', admin.site.urls),
 ]
 
