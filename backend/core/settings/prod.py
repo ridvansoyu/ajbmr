1e67b0faede9da0c7baee133835b782e8ac6eab3
ridvansoyu <ridvansoyu@gmail.com>
2025-08-18 17:40:45 +0300

    fix allowed host

--- backend/core/settings/prod.py
+++ backend/core/settings/prod.py
@@ -15,7 +15,7 @@ CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',') if os.ge
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "ajbmr-paj1.onrender.com",  #w Render domain
+    "ajbmr-paj1.onrender.com"  #w Render domain
 ]
 
 #Configure static & media
